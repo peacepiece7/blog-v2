fc60c3d42cb9018b81301ca90afba1381c337980
peacepiece7 <peacepiece7@naver.com>
2024-06-24 17:46:19 +0900

    mdx test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
 
-export default nextConfig;
+const nextConfig = {
+  // 마크다운 및 MDX 파일을 포함시키기 위해 페이지 확장자 설정
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
+
+const withMDX = createMDX({
+  // 여기에 마크다운 플러그인 추가
+})
+
+export default withMDX(nextConfig)
