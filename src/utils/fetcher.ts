e5ba5200d20638f997919c06f9405f9ac5ab3dee
peacepiece7 <scv7282@gmail.com>
2024-07-11 18:23:59 +0900

    배포 서버 프로토콜 http로 고정 테스트

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -6,7 +6,7 @@ import { headers } from 'next/headers'
 export const fetcher = async <T>(path: string, options?: RequestInit) => {
   const headerList = headers()
   const host = headerList.get('host')
-  const sheme = process.env.NODE_ENV === 'development' ? 'http' : 'https'
-  const url = `${sheme}://${host}${path}`
+  // const sheme = process.env.NODE_ENV === 'development' ? 'http' : 'https'
+  const url = `http://${host}${path}`
   return fetch(url, options).then((res) => res.json() as T)
 }
