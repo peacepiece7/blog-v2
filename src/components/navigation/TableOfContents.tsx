02ea344ea0c6d18a6bf5260f3ff27f1fb0ac13d5
peacepiece7 <peacepiece7@naver.com>
2024-07-19 06:23:57 +0900

    tableOfContents transition 이벤트 제거 (주석으로 남겨둠)

--- src/components/navigation/TableOfContents.tsx
+++ src/components/navigation/TableOfContents.tsx
@@ -12,33 +12,36 @@ export default function TableOfContents(props: {
   const [isActive, setIsActive] = useState(props.isActive)
   const [scopeRef, animate] = useAnimate<HTMLDivElement>()
 
-  useEffect(() => {
-    // if (typeof window === 'undefined' || !tocRef.current) return
-    // tocRef.current.addEventListener('click', (e: any) => {
-    //   console.log('🚀 ~ useEffect ~ e:', e)
-    // }
-
-    scopeRef.current.onclick = (e: MouseEvent) => {
-      // 예외처리하기 UL, LI일 경우
-      if (e.target) {
-        const el = e.target as HTMLElement
-        const li = el.tagName === 'LI' ? el : el.parentElement
-        if (!li) {
-          return alert('LI 태그가 없습니다.')
-        }
-        const nextSiblingEl = li.nextElementSibling
-        if (nextSiblingEl?.tagName === 'UL') {
-          if (nextSiblingEl.classList.contains('inactive-toc-tree')) {
-            nextSiblingEl.classList.remove('inactive-toc-tree')
-            animate(nextSiblingEl, { opacity: 1, y: 5 })
-          } else {
-            nextSiblingEl.classList.add('inactive-toc-tree')
-            animate(nextSiblingEl, { opacity: 0, y: -5 })
-          }
-        }
-      }
-    }
-  }, [])
+  // useEffect(() => {
+  // scopeRef.current.onclick = (e: MouseEvent) => {
+  // 예외처리하기 UL, LI일 경우
+  // if (e.target) {
+  // const el = e.target as HTMLElement
+  // const li = el.tagName === 'LI' ? el : el.parentElement
+  // if (!li) {
+  // return alert('LI 태그가 없습니다.')
+  // }
+  // const nextSiblingEl = li.nextElementSibling
+  // if (nextSiblingEl?.tagName === 'UL') {
+  //   if (nextSiblingEl.classList.contains('inactive-toc-tree')) {
+  //     nextSiblingEl.classList.remove('inactive-toc-tree')
+  //     animate(
+  //       nextSiblingEl,
+  //       { height: 'auto' },
+  //       { ease: 'easeInOut', duration: 0.4 }
+  //     )
+  //   } else {
+  //     nextSiblingEl.classList.add('inactive-toc-tree')
+  //     animate(
+  //       nextSiblingEl,
+  //       { height: 0 },
+  //       { ease: 'easeInOut', duration: 0.4 }
+  //     )
+  //   }
+  // }
+  // }
+  // }
+  // }, [])
 
   return (
     <div ref={scopeRef} className={`${isActive ? 'visible' : 'hidden'}`}>
