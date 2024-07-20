179322cacf7da8d1a23e5f2890d592d1040f5fe6
peacepiece7 <peacepiece7@naver.com>
2024-07-20 17:39:36 +0900

    leftSidebar 토글 아이콘 추가

--- src/components/navigation/LeftSideBar.tsx
+++ src/components/navigation/LeftSideBar.tsx
@@ -6,9 +6,10 @@ import { fetcher } from '@/utils/fetcher'
 import { TreeNode } from '@/app/api/navigation/route'
 import { X_CUSTOM_URL } from '@/constants/server'
 import { headers } from 'next/headers'
-import { FaRegFolderOpen } from 'react-icons/fa'
-import { IoFolderOpenOutline } from 'react-icons/io5'
+import { IoFolderOpenOutline, IoFolderOutline } from 'react-icons/io5'
 import { TiDocument } from 'react-icons/ti'
+import { GoHash } from 'react-icons/go'
+import Link from 'next/link'
 
 export async function LeftSideBar() {
   const headerList = headers()
@@ -18,12 +19,14 @@ export async function LeftSideBar() {
   const isPostPage = url.includes('posts')
 
   const tocRes = await fetcher<{ tocTree: Heading[] }>(
-    `/api/table-of-contents?url=${url}`
+    `/api/table-of-contents?url=${url}`,
+    { cache: 'no-cache' }
   )
-  const navRes = await fetcher<{ navTree: TreeNode[] }>('/api/navigation')
+
+  const navRes = await fetcher<{ navTree: TreeNode[] }>(`/api/navigation`, {})
 
   return (
-    <nav className='col-start-1 col-end-2 row-start-2 row-end-4 w-fit min-w-fit max-w-[50rem] px-10 overflow-y-scroll'>
+    <nav className='col-start-1 col-end-2 row-start-2 row-end-4 w-fit min-w-fit max-w-[50rem] px-10 overflow-y-scroll pt-4'>
       <Navigation isActive={!isPostPage}>
         {createNavElements(navRes.navTree, [], 0)}
       </Navigation>
@@ -48,21 +51,44 @@ const createNavElements = (
       nodelinks.push((idx + 1).toString())
       node.link = nodelinks.join('/')
     }
+    // children 중 route.path와 같은 것이 있으면 open으로 상태 변경
+    // main/tree/docker/example/architecture 이런 느낌으로 deepth와 path를 비교해서 open 상태로 변경
     return (
       <li
         key={node.text}
         data-path={dataPath.join('-')}
         data-deepth={deepth}
         data-leaf={node.leafNode ? 1 : 0}
-        className='list-disc ml-6'
+        className={`list-disc ml-6 overflow-hidden`}
       >
         {node.leafNode && node.link ? (
-          <a href={node.link}>{node.text}</a>
+          <span
+            className='flex items-center
+           hover:bg-gray-400 hover:bg-opacity-10
+          '
+          >
+            <TiDocument className='mr-2 min-w-6 min-h-6 w-6 h-6' />
+            <Link href={node.link}>{node.text}</Link>
+          </span>
         ) : (
-          <>
-            <span>{node.text}</span>
-            <ul>{createNavElements(node.children, dataPath, deepth + 1)}</ul>
-          </>
+          <div className='inactive-tree-node'>
+            <span
+              className='
+              flex items-center cursor-pointer
+              hover:bg-gray-400 hover:bg-opacity-10
+            '
+            >
+              <IoFolderOpenOutline className='folder-open mr-2 min-w-6 min-h-6 w-6 h-6' />
+              <IoFolderOutline className='folder-close mr-2 min-w-6 min-h-6 w-6 h-6' />
+              <p>{node.text}</p>
+            </span>
+            <ul
+              // ${deepth === 0 ? 'hidden inactive-tree-node' : ''}
+              className={`hidden list-disc ml-6 overflow-hidden`}
+            >
+              {createNavElements(node.children, dataPath, deepth + 1)}
+            </ul>
+          </div>
         )}
       </li>
     )
@@ -83,18 +109,21 @@ function createTOCElements(
     const text = heading.children
       .flatMap((node) => ('value' in node ? node.value : ''))
       .join('')
-    const formattedText = text.replace(/\s+/g, '_')
+    const hashTag = text.replace(/\s+/g, '_').toLowerCase()
     const li = React.createElement(
       'li',
       {
         key: `li-${index}`,
-        className: 'list-disc ml-6',
+        className: 'flex items-center list-disc ml-6 overflow-hidden',
         'data-deepth': depth,
       },
+      React.createElement(GoHash, {
+        className: 'mr-2 min-w-6 min-h-6 w-6 h-6',
+      }),
       React.createElement(
-        'a',
+        Link,
         {
-          href: `#${formattedText}`,
+          href: `#${hashTag}`,
         },
         text
       )
@@ -108,12 +137,11 @@ function createTOCElements(
       }
     }
 
-    // const parentUl = stack[stack.length - 1].element
     stack[stack.length - 1].children.push(li)
 
     const newUl = React.createElement('ul', {
       key: `ul-${index}`,
-      className: 'list-disc ml-6',
+      className: 'list-disc ml-6 overflow-hidden',
     })
     stack.push({ element: newUl, depth, children: [] as React.ReactElement[] })
   })
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,21 +1,64 @@
 'use client'
-import { usePathname } from 'next/navigation'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useAnimate } from 'framer-motion'
 
-export default function Navigation(props: {
-  children: React.ReactNode
-  isActive: boolean
-}) {
-  const pathname = usePathname()
-  const [isActive, setIsActive] = useState(props.isActive)
-  const ulRef = useRef<HTMLUListElement>(null)
+export default function Navigation(
+  props: Readonly<{
+    children: React.ReactNode
+    isActive: boolean
+  }>
+) {
+  const [isActive] = useState(props.isActive)
+  const [scopeRef, animate] = useAnimate<HTMLUListElement>()
 
   useEffect(() => {
-    setIsActive(!pathname.includes('posts'))
-  }, [pathname])
+    scopeRef.current.onclick = (e: MouseEvent) => {
+      // 예외처리하기 UL, LI일 경우
+      if (e.target) {
+        const el = e.target as HTMLElement
+        // if achor tag is clicked, do nothing
+        if (el.tagName === 'A') {
+          return
+        }
+
+        const li = el.tagName === 'LI' ? el : el.closest('li')
+
+        if (!li) {
+          return alert('LI 태그가 없습니다.')
+        }
+
+        const divElement = li.querySelector('div') as HTMLDivElement
+        const firstUlChild = divElement.querySelector('ul') as HTMLUListElement
+        if (divElement?.tagName === 'DIV') {
+          if (divElement.classList.contains('inactive-tree-node')) {
+            divElement.classList.remove('inactive-tree-node')
+
+            firstUlChild.style.height = '0'
+            firstUlChild.classList.remove('hidden')
+            animate(
+              firstUlChild,
+              { height: 'auto' },
+              { ease: 'easeInOut', duration: 0.4 }
+            )
+          } else {
+            divElement.classList.add('inactive-tree-node')
+
+            animate(
+              firstUlChild,
+              { height: 0 },
+              { ease: 'easeInOut', duration: 0.4 }
+            )
+          }
+        }
+      }
+    }
+  }, [])
 
   return (
-    <ul ref={ulRef} className={`${isActive ? 'visible' : 'hidden'}`}>
+    <ul
+      ref={scopeRef}
+      className={`inactive-node-tree ${isActive ? 'visible' : 'hidden'}`}
+    >
       {props.children}
     </ul>
   )
