89be36670cb45501faad94d5a42ea5f1fdf4f783
Tae Uk <73880776+peacepiece7@users.noreply.github.com>
2024-07-29 19:13:11 +0900

    page.tsx 업데이트

--- src/app/posts/@contents/diary/[pageId]/page.tsx
+++ src/app/posts/@contents/diary/[pageId]/page.tsx
@@ -7,7 +7,7 @@ export default async function PostPage({
   params: { pageId: string }
 }>) {
   const MDXPage = await new Promise<any>((resolve) => {
-    const postPath = getPostFullPath("dairy", "[pageId]")
+    const postPath = getPostFullPath("diary", "[pageId]")
     const fileNames = getFileNamesSafely(postPath, "mdx")
     import(`./${fileNames[parseInt(params?.pageId) - 1 || 0]?.name}`).then(
       (module) => resolve(module.default)
